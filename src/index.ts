4ee13a0efcb07c6bee2e70a42878a16b7dd6deae
Shubham Vishwakarma <shubham.vishwakarma@justdial.com>
2020-06-29 12:45:46 +0530

    Response Handling

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { capture } from "./screenshot";
+import { capture } from './screenshot'
 
 //temp file to generate screenshots
 const urls = [
@@ -8,10 +8,33 @@ const urls = [
 	'https://marketplace.betatesting.jdseller.com/vendor/manage-products-genio/NW9tbm9RRWxtR09seVJiNUpRVmVKVkFyMjBLaGpJSFFPWXlSaVc5cVErQkx4OGFrd2JGdVVFMVdlMVAxZUxHNw==/products'
 ];
 
-(async function (){
-	let promiseArr:any[] = [];
-	urls.forEach((url) => {
-		promiseArr.push(capture(url))
-	})
-	await Promise.all(promiseArr);
+const urlResponseObj = {
+	isSuccess: true,
+	id: 'e08eec07-5604-4eb0-8f75-813e64a8f4df',
+	websiteLinks: urls.join(','),
+	cb: 'http://localhost:3000/screenshots-public/cb?themeId=1234',
+	imageUrls: null,
+	ssOptions: '{"a":"sdds"}',
+	status: 'PENDING',
+	type: 'THEME',
+	creationTstamp: '2020-06-29T06:12:10.000Z',
+	lastModifiedTstamp: '0000-00-00 00:00:00',
+	ssServerIp: null,
+	token: '3d047304-b0aa-11ea-9bbd-e03676ca53b0',
+	errorMessage: null,
+	ssAllocationTstamp: '0000-00-00 00:00:00',
+};
+
+(async function () {
+	let promiseArr: any[] = []
+	let {
+		websiteLinks
+	} = urlResponseObj;
+	let ssOptions = {};
+	try{
+		ssOptions = JSON.parse(urlResponseObj.ssOptions) || {}
+	} catch(e){}
+	const urlsToCapture = websiteLinks ? websiteLinks.split(","): [];
+	promiseArr = urlsToCapture.map((url) => capture(url, ssOptions))
+	await Promise.all(promiseArr)
 })()
--- src/screenshot.ts
+++ src/screenshot.ts
@@ -4,7 +4,7 @@ import { v4 as uuid } from "uuid";
 import { autoScroll } from "./utils/autoScroll";
 
 
-async function generate(browserInst: Browser, url:string): Promise<void> {
+async function generate(browserInst: Browser, url:string, options: object): Promise<void> {
 	const newPage = await browserInst.newPage();
 	await newPage.goto(url, {
 		waitUntil: 'networkidle0'
@@ -15,13 +15,15 @@ async function generate(browserInst: Browser, url:string): Promise<void> {
 	await newPage.screenshot({
 		path: screenshotPath,
 		fullPage: true,
-		type: 'png'
+		type: 'png',
+		...options
 	})
 	console.log(`Generated Screenshot for ${url} \n ${screenshotPath}`)
 }
 
-export async function capture(url: string): Promise<void> {
+//change options to predefined type
+export async function capture(url: string, options: object): Promise<void> {
 	const browserInst = await getBrowserInstance({});
-	console.log('Started for URL', url)
-	generate(browserInst, url);
+	console.log('Started for URL', url, options)
+	generate(browserInst, url, options);
 }
